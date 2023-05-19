9099712330fa1ec421a4ba2c29aaf5c0665f57f0
kualv <129527207+kualv@users.noreply.github.com>
2023-05-19 12:01:16 -0400

    Add files via upload

---
+++ ej2.py
@@ -0,0 +1,14 @@
+void main() {
+    List<int> numeros = [14,2,5,3,9]
+    List<int> num1 = [2,4,6,8,10]
+    List<int> num2 = []
+    int resta = 0;
+    for(int numero in numeros) {
+        resta -= numero;
+    }
+    print(resta)
+}
+numero
+
+
+
